e6ed0b393352aba61c90a4db3fa21d3eb6c77efd
Rizqy Hidayat <rizqy22@gmail.com>
2023-09-16 20:22:15 +0700

    add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let title = $page.status === 404 ? 'Route Not Found' : 'Something Went Bad';
+</script>
+
+<svelte:head>
+	<title>{title} &ndash; Rizqy Hidayat</title>
+</svelte:head>
+
+<div class="py-16">
+	<h1 class="text-5xl font-mono font-bold tracking-tighter md:leading-snug">{title}</h1>
+</div>
