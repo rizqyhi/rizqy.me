d8645672a3f16dfc1746b6638fbe1ae0fa48216d
Rizqy Hidayat <rizqy22@gmail.com>
2025-03-22 05:37:18 +0700

    feat: set build format to preserve

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  build: {
+    format: "preserve",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
